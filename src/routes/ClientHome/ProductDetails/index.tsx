9eb61b6ecdc156fb0933be5c88d488d282811ece
José Carlos de Souza <jcjrta@gmail.com>
2024-03-03 09:55:55 -0300

    Adding React Router DOM

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -1,9 +1,8 @@
 import './styles.css';
-import ButtonInverse from "../../components/ButtonInverse";
-import ButtonPrimary from "../../components/ButtonPrimary";
-import HeaderClient from "../../components/HeaderClient";
-import ProductDetailsCard from "../../components/ProductDetailsCard";
-import { ProductDTO } from '../../components/models/product';
+import ButtonInverse from "../../../components/ButtonInverse";
+import ButtonPrimary from "../../../components/ButtonPrimary";
+import ProductDetailsCard from "../../../components/ProductDetailsCard";
+import { ProductDTO } from '../../../components/models/product';
 
 const product : ProductDTO = {
   id : 2,
@@ -31,7 +30,6 @@ export default function ProductDetails(){
     return(
 
         <>
-        <HeaderClient/>
         <main>
           <section id="product-details-section" className="dsc-container">
            <ProductDetailsCard product = { product }/>
