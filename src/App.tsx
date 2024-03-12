eaa7c6e55ffbfd4dfb0ec4724061bcd038356a43
José Carlos de Souza <jcjrta@gmail.com>
2024-03-11 22:45:21 -0300

    Cart mock data

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Catalog from './routes/ClientHome/Catalog';
 import ProductDetails from './routes/ClientHome/ProductDetails';
 import ClientHome from './routes/ClientHome';
+import Cart from './routes/ClientHome/Cart';
 
 export default function App() {
   
@@ -14,6 +15,7 @@ export default function App() {
       <Route index element={<Catalog />}></Route>
       <Route path='catalog' element={<Catalog />}></Route>
       <Route path='product-details/:productId' element={<ProductDetails />}></Route>
+      <Route path='cart' element={<Cart />}></Route>
     </Route> 
     <Route path="*" element={<Navigate to="/" />}></Route>
   </Routes>
--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -6,13 +6,15 @@ import ButtonNextPage from '../../../components/ButtonNextPage';
 import * as productService from '../../../services/product-service';
 import { useEffect, useState } from 'react';
 import { ProductDTO } from '../../../components/models/product';
+
 //import axios from 'axios';
 
   export default function Catalog(){
-
+    
     const [products, setProducts] = useState<ProductDTO[]>([]);
 
     useEffect(() => {
+     
       productService.findAll()
         .then(response => {
           setProducts(response.data.content);
