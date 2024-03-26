55cbb08af6a757b3f05589f78646afbebc2fd320
José Carlos de Souza <jcjrta@gmail.com>
2024-03-25 23:38:36 -0300

    Form TextArea

--- src/routes/Admin/ProductsForm/index.tsx
+++ src/routes/Admin/ProductsForm/index.tsx
@@ -4,6 +4,7 @@ import { ChangeEvent, useEffect, useState } from 'react';
 import FormInput from '../../../components/FormInput';
 import * as forms from '../../../utils/forms';
 import * as productService from '../../../services/product-service';
+import FormTextArea from '../../../components/FormTextArea';
 
 export default function ProductForm() {
 
@@ -45,6 +46,18 @@ export default function ProductForm() {
       type: "text",
       placeholder: "Imagem",
     },
+    description: {
+      value: "",
+      id: "description",
+      name: "description",
+      type: "text",
+      placeholder: "Descrição",
+      validation: function(value: string) {
+        return /^.{10,}$/.test(value);
+      },
+      message: "A descrição deve ter pelo menos 10 caracteres"
+
+    }
     });
 
     useEffect(() => {
@@ -99,6 +112,15 @@ export default function ProductForm() {
                         onTurnDirty={handleTurnDirty}
                         onChange={handleInputChange}
                     />     
+                </div>
+                <div>
+                <FormTextArea
+                        {...formData.description }
+                        className="dsc-form-control dsc-textarea" 
+                        onTurnDirty={handleTurnDirty}
+                        onChange={handleInputChange}
+                    />   
+                    <div className='dsc-form-error'>{formData.description.message}</div>  
                 </div>
               
               </div>
