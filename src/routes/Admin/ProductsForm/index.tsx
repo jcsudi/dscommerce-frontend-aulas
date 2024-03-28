06834afe1286ba45eab22b446aa4f1304d0f0ec9
José Carlos de Souza <jcjrta@gmail.com>
2024-03-27 22:25:21 -0300

    Backend errors

--- src/routes/Admin/ProductsForm/index.tsx
+++ src/routes/Admin/ProductsForm/index.tsx
@@ -125,7 +125,14 @@ export default function ProductForm() {
       request
           .then(() => {
             navigate("/admin/products");
+          })
+          .catch(error => {
+            const newInputs = forms.setBackendErrors(formData, error.response.data.errors);
+            setFormData(newInputs);
+
           });
+          
+
     }
 
     return(
